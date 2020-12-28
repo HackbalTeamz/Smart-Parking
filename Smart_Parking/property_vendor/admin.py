1124164269ef64c0ba1b3c26be45df178be8917d
Akhil MS <akhilsree001@gmail.com>
2020-12-28 20:14:21 +0530

    register models to admin panel

--- Smart_Parking/property_vendor/admin.py
+++ Smart_Parking/property_vendor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import property,pslot,userProfile
+# Register your models here.
+admin.site.register(property)
+admin.site.register(pslot)
+admin.site.register(userProfile)
