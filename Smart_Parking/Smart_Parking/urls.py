245308aa8a9e3111d56fa6699a305d69fb13a5c7
Akhil MS <akhilsree001@gmail.com>
2020-12-29 20:01:47 +0530

    added search url,view,template

--- Smart_Parking/Smart_Parking/urls.py
+++ Smart_Parking/Smart_Parking/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',pv.index,name="index"),
+    path('search',pv.search,name="search"),
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- Smart_Parking/public/views.py
+++ Smart_Parking/public/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from property_vendor.models import property,pslot,userProfile
 
 # Create your views here.
 def index(request):
 	return render(request,'public/index.html')
+
+def search(request):
+	results=property.objects.all()
+	return render(request,'public/search.html',{'results':results})
